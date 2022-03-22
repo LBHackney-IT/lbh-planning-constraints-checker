2f088f8b2ac2c7ba88ad1f06e1942b52c5d3e581
Sandrine Balley <sandrine.balley@hackney.gov.uk>
2022-03-22 18:19:02 +0100

    replace getIntoView with scrollBy

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ document.getElementById("start-button").onclick = function(){
   console.log('first click');
   document.getElementById("location-search").hidden = false;
   document.getElementById("start-button").style.display = "none";
-  document.getElementById("search").scrollIntoView(false);
-  console.log('document.getElementById("search").scrollIntoView(false);');
+  // document.getElementById("search").scrollIntoView(false);
+  // console.log('document.getElementById("search").scrollIntoView(false);');
   // window.parent.scrollBy(0,200);
 };
 
@@ -120,8 +120,10 @@ function GetAddressesViaProxy() {
           //console.log('uprn = ' + selectedUPRN);
           showAddressDetails(selectedAddressDetails);
         });  
-        document.getElementById("selectedAddress").scrollIntoView(false);
-        console.log('getElementById("selectedAddress").scrollIntoView(false)')
+        window.scrollBy(0,200);
+        console.log('scroll by 200;');
+        // document.getElementById("selectedAddress").scrollIntoView(false);
+        // console.log('getElementById("selectedAddress").scrollIntoView(false)')
       }
     }
   }).catch(error => {
@@ -189,9 +191,10 @@ function showPlanningInfoButton(selectedUPRN){
   };
   //Scroll down to show the show results button
   // document.getElementById('iframe-checker-app').contentWindow.document.getElementById("show-results-button").scrollIntoView();
-  // window.parent.scrollBy(0,200);
-  document.getElementById("show-results-button").scrollIntoView(false);
-  console.log('getElementById("show-results-button").scrollIntoView(false)')
+  window.scrollBy(0,400);
+  console.log('scroll by 400;');
+  // document.getElementById("show-results-button").scrollIntoView(false);
+  // console.log('getElementById("show-results-button").scrollIntoView(false)')
 }
 
 function loadPlanningConstraints(selectedUPRN){
@@ -362,16 +365,17 @@ function loadPlanningConstraints(selectedUPRN){
         setTimeout(() => {document.getElementById("map-header").innerHTML = "<h3>Map view:</h3>";}, 4500);
         setTimeout(() => {document.getElementById("map-link").innerHTML = ""; }, 4500);
         //Scroll down to show the map
-        // document.getElementById('iframe-checker-app').contentWindow.document.getElementById("div-map-iframe").scrollIntoView();
-        document.getElementById("map-iframe").scrollIntoView();
-        console.log('getElementById("map-iframe").scrollIntoView()')
+        window.scrollBy(0,200);
+        console.log('scroll by 200;');
+        // document.getElementById("map-iframe").scrollIntoView();
+        // console.log('getElementById("map-iframe").scrollIntoView()')
         //window.parent.scrollBy(0,200);
       }      
       //Scroll down to see the results list
-      // document.getElementById('iframe-checker-app').contentWindow.document.getElementById("results").scrollIntoView();
-      // window.parent.scrollBy(0,200);
-      document.getElementById("map-link-button").scrollIntoView(false);
-      console.log('getElementById("map-link-button").scrollIntoView(false)')
+      window.scrollBy(0,400);
+      console.log('scroll by 400;');
+      // document.getElementById("map-link-button").scrollIntoView(false);
+      // console.log('getElementById("map-link-button").scrollIntoView(false)')
     })
     .catch((error) => {
       //Catch geoserver error
